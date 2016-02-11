810806af20cbbe1f29f56ccdd4def1cfaae8f261
Chris Houghton and trevor elvey <trevor.elvey@gmail.com>
2016-02-11 13:50:22 -0800

    functioning mvp

--- src/main/java/CoinSort.java
+++ src/main/java/CoinSort.java
@@ -1,11 +1,34 @@
 import java.util.HashMap;
-
+import java.util.Map;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
 import static spark.Spark.*;
 
 public class CoinSort {
     public static void main(String[] args) {
+      String layout = "templates/layout.vtl";
+
+      get("/", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("template", "templates/home.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      get("/return", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/return.vtl");
+
+      String stringCoins= request.queryParams("userInput");
+      Integer coins = Integer.parseInt(stringCoins);
+
+      String change;
+      change = checkChange(coins);
+
+      model.put("userInput", change);
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+
     }
     public static String checkChange(Integer userInput) {
       String result = "";
